d230df927eb25ba3ad4321905a663ba568475aee
Denis Veselov <progr-m@yandex.ru>
2020-10-03 17:31:45 +0300

    upd

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='textdatasetcleaner',
-    version='1.0.0',
+    version='0.0.1',
     author='Denis Veselov',
     description='Pipeline for cleaning (preprocessing/normalizing) text datasets',
     url='https://github.com/TextDatasetCleaner/TextDatasetCleaner',
